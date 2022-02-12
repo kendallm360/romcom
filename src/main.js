1781c31634683c33eb8a7b0d05aff67a1c69b482
Kendall McGree <mcgreekendall@gmail.com>
2022-02-11 18:48:10 -0800

    Complete Iteration 2

--- src/main.js
+++ src/main.js
@@ -13,74 +13,82 @@ var currentCover = Cover
 // Create your event handlers and other functions here 👇
 
 
-// We've provided one function to get you started
-// function getRandomIndex(array) {
-//   return Math.floor(Math.random() * array.length);
-// }
 
-// class RomCom {
-//   constructor() {
-//
-//   }
-// }
-
-
-
-var randomCoverButton = document.querySelector(".random-cover-button");
-var hideHomeView = document.querySelector(".home-view");
+var homeView = document.querySelector(".home-view");
 var showFormView = document.querySelector(".form-view");
+var savedView= document.querySelector(".saved-view");
+var randomCoverButton = document.querySelector(".random-cover-button");
+var homeButton = document.querySelector(".home-button");
+var saveCoverButton = document.querySelector(".save-cover-button");
 var currentTitle = document.querySelector('.cover-title');
 var currentImage = document.querySelector('.cover-image');
 var currentTagLine1 = document.querySelector('.tagline-1');
 var currentTagLine2 = document.querySelector('.tagline-2');
-var makeCoverButton = document.querySelector(".make-new-button");
+
+
 
 window.addEventListener("click", clickHandler);
-window.addEventListener("load", randomizeCover);
-// randomCoverButton.addEventListener("click", changeButton);
-//hideHomeView.addEventListener("click", changeView)
-// makeCoverButton.addEventListener("click", showCustomCover)
+window.addEventListener("load", randomCover);
+
 
 //redo for hide element
-function hideElement(element) {
-  document.querySelector(`.${element}`).classList.add("hidden");
-}
+// function hideElement(element) {
+//   document.querySelector(`.${element}`).classList.add("hidden");
+// }
 
 
 function clickHandler(event) {
   if (event.target.classList.contains("make-new-button")){
-    showCustomCover()
+    makeNewCover()
   } else if (event.target.classList.contains("random-cover-button")){
-    changeButton()
+    randomCover()
+  } else if (event.target.classList.contains("view-saved-button")){
+    viewSavedCovers()
+  } else if (event.target.classList.contains("home-button")){
+    viewHome()
   }
 }
 
 
- function randomizeCover() {
-   currentTitle.innerText = getRandomBookInfo(titles);
-   currentImage.src = getRandomBookInfo(covers);
-   currentTagLine1.innerText = getRandomBookInfo(descriptors);
-   currentTagLine2.innerText = getRandomBookInfo(descriptors);
- }
-
-function showCustomCover() {
+function makeNewCover() {
   //hideHomeView.innerHTML += ".view home-view hidden";
   //showFormView.innerHTML = ""
-  hideHomeView.classList.add("hidden")
-  hideElement("home-view");
+  homeView.classList.add("hidden");
+  randomCoverButton.classList.add("hidden");
+  saveCoverButton.classList.add("hidden");
+  //hideElement("home-view");......referenced in line33-34(hide elementfunction)
   showFormView.classList.remove("hidden");
+  homeButton.classList.remove("hidden");
 }
 
+function viewSavedCovers() {
+  homeView.classList.add("hidden");
+  showFormView.classList.add("hidden");
+  savedView.classList.remove("hidden");
+  randomCoverButton.classList.add("hidden");
+  saveCoverButton.classList.add("hidden");
+  homeButton.classList.remove("hidden");
+}
+
+function viewHome() {
+  homeView.classList.remove("hidden");
+  showFormView.classList.add("hidden");
+  savedView.classList.remove("hidden");
+  randomCoverButton.classList.remove("hidden");
+  saveCoverButton.classList.remove("hidden");
+  homeButton.classList.add("hidden");
+}
+
+
+
 // //lines 39-46(next8) invokes all the randomness at button click random
-function changeButton() {
+function randomCover() {
 currentTitle.innerText = getRandomBookInfo(titles);
-//currentImageSrc.innerText = getRandomBookInfo(covers);
 currentImage.src = getRandomBookInfo(covers);
 currentTagLine1.innerText = getRandomBookInfo(descriptors);
 currentTagLine2.innerText = getRandomBookInfo(descriptors);
 }
 
-//hideHomeView.innerHTML = ".view home-view hidden";
 //line for randomness below
 function getRandomBookInfo(bookInfo) {
   return bookInfo[Math.floor(Math.random() * bookInfo.length)];
