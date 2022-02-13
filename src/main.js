ade97e354a08de7161d4e571f24fba8353af4126
Kendall McGree <mcgreekendall@gmail.com>
2022-02-13 14:32:45 -0800

    Update the Save Covers Function

--- src/main.js
+++ src/main.js
@@ -1,25 +1,27 @@
 // Create variables targetting the relevant DOM elements here 👇
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover(
-    "http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg",
-    "Sunsets and Sorrows",
+var savedCovers = [new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg",
+"Sunsets and Sorrows",
     "sunsets",
     "sorrows"
   ),
 ];
 
-//var currentCover = Cover
+var currentCover
 
 // Add your event listeners here 👇
 
 // Create your event handlers and other functions here 👇
-
 var homeView = document.querySelector(".home-view");
 var showFormView = document.querySelector(".form-view");
 var savedView = document.querySelector(".saved-view");
+
+var coverGallery = document.querySelector(".saved-covers-section")
+
 var randomCoverButton = document.querySelector(".random-cover-button");
+var makeNewButton = document.querySelector(".make-new-button")
+var galleryButton = document.querySelector(".view-saved-button")
 var homeButton = document.querySelector(".home-button");
 var saveCoverButton = document.querySelector(".save-cover-button");
 var currentTitle = document.querySelector(".cover-title");
@@ -31,15 +33,7 @@ var userTitle = document.getElementById("title");
 var userDescriptor1 = document.getElementById("descriptor1");
 var userDescriptor2 = document.getElementById("descriptor2");
 var createBookButton = document.querySelector(".create-new-book-button");
-var currentCover = new Cover(
-  userCover.value,
-  userTitle.value,
-  userDescriptor1.value,
-  userDescriptor2.value
-);
-//var currentCover = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value)
 
-window.addEventListener("click", clickHandler);
 window.addEventListener("load", randomCover);
 
 function showElement(elementVar) {
@@ -50,43 +44,27 @@ function hideElement(elementVar) {
   elementVar.classList.add("hidden");
 }
 
-function clickHandler(event) {
-  if (event.target.classList.contains("make-new-button")) {
-    makeNewCover();
-  } else if (event.target.classList.contains("random-cover-button")) {
-    randomCover();
-  } else if (event.target.classList.contains("view-saved-button")) {
-    viewSavedCovers();
-  } else if (event.target.classList.contains("home-button")) {
-    viewHome();
-  } else if (event.target.classList.contains("create-new-book-button")) {
-    createNewBook();
-  } else if (event.target.classList.contains("save-cover-button")) {
-    saveCover();
-  }
-}
-
-//var book1 = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value)
+makeNewButton.addEventListener("click", makeNewCover)
+randomCoverButton.addEventListener("click", randomCover)
+galleryButton.addEventListener("click", viewSavedCovers)
+homeButton.addEventListener("click", viewHome)
+createBookButton.addEventListener("click", createNewBook)
+saveCoverButton.addEventListener("click", saveCover)
 
 function createNewBook() {
   event.preventDefault();
-  var currentCover = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value);
+  var newTitle = userTitle.value
+  var newCover = userCover.value
+  var newDescriptor1 =  userDescriptor1.value
+  var newDescriptor2= userDescriptor2.value
+  displayCover(newTitle, newCover, newDescriptor1, newDescriptor2)
   //event.preventDefault();
   //console.log(currentCover);
-    currentTitle.innerText = currentCover.title;
-    currentImage.src = currentCover.cover;
-    currentTagLine1.innerText = currentCover.tagline1;
-    currentTagLine2.innerText = currentCover.tagline2;
     viewHome();
     titles.push(userTitle.value);
     covers.push(userCover.value);
     descriptors.push(userDescriptor1.value);
     descriptors.push(userDescriptor2.value);
-return Cover
-
-
-// console.log(currentCover);
-// savedCovers.push(currentCover);
 }
 
 function makeNewCover() {
@@ -98,36 +76,21 @@ function makeNewCover() {
 }
 
 function saveCover() {
-  var currentCover = new Cover(
-    //currentTitle.innerText
-    // userCover.value,
-    //userTitle.value,
-    currentImage.src,
-    currentTitle.innerText,
-    currentTagLine1.innerText,
-    currentTagLine2.innerText,
-    // userDescriptor1.value,
-    // userDescriptor2.value,
-  );
-  // console.log(currentCover);
-  savedCovers.push(currentCover);
-  // currentCover(currentTitle.innerText)
-  console.log(savedCovers);
-  //   savedCovers.push(book1);
-  //   // covers.push(userCover.value);
-  //   // descriptors.push(userDescriptor1.value);
-  //   // descriptors.push(userDescriptor2.value);
-  //return Cover
+  if (!savedCovers.includes(currentCover)){
+    savedCovers.push(currentCover);
+  }
+
+console.log(savedCovers)
 }
 
 function viewSavedCovers() {
+
   hideElement(homeView);
   hideElement(showFormView);
   showElement(savedView);
   hideElement(randomCoverButton);
   hideElement(saveCoverButton);
   showElement(homeButton);
-  return savedCovers;
 }
 
 function viewHome() {
@@ -140,12 +103,33 @@ function viewHome() {
 }
 
 function randomCover() {
-  currentTitle.innerText = getRandomBookInfo(titles);
-  currentImage.src = getRandomBookInfo(covers);
-  currentTagLine1.innerText = getRandomBookInfo(descriptors);
-  currentTagLine2.innerText = getRandomBookInfo(descriptors);
+  var newTitle = titles[getRandomBookInfo(titles)];
+  var newCover = covers[getRandomBookInfo(covers)];
+  var newDescriptor1 = descriptors[getRandomBookInfo(descriptors)];
+  var newDescriptor2= descriptors[getRandomBookInfo(descriptors)];
+  displayCover(newTitle, newCover, newDescriptor1, newDescriptor2)
+}
+
+function displayCover(newTitle, newCover, newDescriptor1, newDescriptor2) {
+  currentCover = new Cover(newTitle, newCover, newDescriptor1, newDescriptor2);
+  currentTitle.innerText = newTitle
+  currentImage.src = newCover
+  currentTagLine1.innerText = newDescriptor1
+  currentTagLine2.innerText = newDescriptor2
 }
 
 function getRandomBookInfo(bookInfo) {
-  return bookInfo[Math.floor(Math.random() * bookInfo.length)];
+  return Math.floor(Math.random() * bookInfo.length);
 }
+
+
+function duplicates() {
+console.log(savedCovers);
+}
+
+function pushToArray() {
+  covers.push(currentCover.cover);
+  titles.push(currentCover.title);
+  descriptors.push(currentCover.tagline1);
+  descriptors.push(currentCover.tagline2);
+};
