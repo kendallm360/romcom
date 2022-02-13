114aa292208dcddc243021320e918652f199231c
Kendall McGree <mcgreekendall@gmail.com>
2022-02-12 16:21:46 -0800

    Update Saving Covers

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 // Create variables targetting the relevant DOM elements here 👇
 
-
 // We've provided a few variables below
 var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+  new Cover(
+    "http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg",
+    "Sunsets and Sorrows",
+    "sunsets",
+    "sorrows"
+  ),
 ];
 
 //var currentCover = Cover
-
 
 // Add your event listeners here 👇
 
@@ -15,70 +18,76 @@ var savedCovers = [
 
 var homeView = document.querySelector(".home-view");
 var showFormView = document.querySelector(".form-view");
-var savedView= document.querySelector(".saved-view");
+var savedView = document.querySelector(".saved-view");
 var randomCoverButton = document.querySelector(".random-cover-button");
 var homeButton = document.querySelector(".home-button");
 var saveCoverButton = document.querySelector(".save-cover-button");
-var currentTitle = document.querySelector('.cover-title');
-var currentImage = document.querySelector('.cover-image');
-var currentTagLine1 = document.querySelector('.tagline-1');
-var currentTagLine2 = document.querySelector('.tagline-2');
-var userCover = document.getElementById('cover');
-var userTitle = document.getElementById('title');
-var userDescriptor1 = document.getElementById('descriptor1');
-var userDescriptor2 = document.getElementById('descriptor2');
-var createBookButton = document.querySelector('.create-new-book-button');
-var currentCover = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value)
+var currentTitle = document.querySelector(".cover-title");
+var currentImage = document.querySelector(".cover-image");
+var currentTagLine1 = document.querySelector(".tagline-1");
+var currentTagLine2 = document.querySelector(".tagline-2");
+var userCover = document.getElementById("cover");
+var userTitle = document.getElementById("title");
+var userDescriptor1 = document.getElementById("descriptor1");
+var userDescriptor2 = document.getElementById("descriptor2");
+var createBookButton = document.querySelector(".create-new-book-button");
+var currentCover = new Cover(
+  userCover.value,
+  userTitle.value,
+  userDescriptor1.value,
+  userDescriptor2.value
+);
 //var currentCover = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value)
-
-
 
 window.addEventListener("click", clickHandler);
 window.addEventListener("load", randomCover);
 
-
- function showElement(elementVar) {
-  elementVar.classList.remove("hidden")
+function showElement(elementVar) {
+  elementVar.classList.remove("hidden");
 }
 
- function hideElement(elementVar) {
-   elementVar.classList.add("hidden")
+function hideElement(elementVar) {
+  elementVar.classList.add("hidden");
 }
 
 function clickHandler(event) {
-  if (event.target.classList.contains("make-new-button")){
-    makeNewCover()
-  } else if (event.target.classList.contains("random-cover-button")){
-    randomCover()
-  } else if (event.target.classList.contains("view-saved-button")){
-    viewSavedCovers()
-  } else if (event.target.classList.contains("home-button")){
-    viewHome()
-  } else if (event.target.classList.contains("create-new-book-button")){
-    createNewBookButton()
-  } else if (event.target.classList.contains("save-cover-button")){
-    saveCover()
+  if (event.target.classList.contains("make-new-button")) {
+    makeNewCover();
+  } else if (event.target.classList.contains("random-cover-button")) {
+    randomCover();
+  } else if (event.target.classList.contains("view-saved-button")) {
+    viewSavedCovers();
+  } else if (event.target.classList.contains("home-button")) {
+    viewHome();
+  } else if (event.target.classList.contains("create-new-book-button")) {
+    createNewBook();
+  } else if (event.target.classList.contains("save-cover-button")) {
+    saveCover();
   }
 }
 
 //var book1 = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value)
 
-
-function createNewBookButton() {
+function createNewBook() {
   event.preventDefault();
-  var currentCover = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value)
-  currentTitle.innerText = currentCover.title;
-  currentImage.src = currentCover.cover;
-  currentTagLine1.innerText = currentCover.tagline1;
-  currentTagLine2.innerText = currentCover.tagline2;
-  viewHome()
-  titles.push(userTitle.value);
-  covers.push(userCover.value);
-  descriptors.push(userDescriptor1.value);
-  descriptors.push(userDescriptor2.value);
-  savedCovers.push(currentCover);
-}
+  var currentCover = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value);
+  //event.preventDefault();
+  //console.log(currentCover);
+    currentTitle.innerText = currentCover.title;
+    currentImage.src = currentCover.cover;
+    currentTagLine1.innerText = currentCover.tagline1;
+    currentTagLine2.innerText = currentCover.tagline2;
+    viewHome();
+    titles.push(userTitle.value);
+    covers.push(userCover.value);
+    descriptors.push(userDescriptor1.value);
+    descriptors.push(userDescriptor2.value);
+return Cover
 
+
+// console.log(currentCover);
+// savedCovers.push(currentCover);
+}
 
 function makeNewCover() {
   hideElement(homeView);
@@ -88,12 +97,27 @@ function makeNewCover() {
   showElement(homeButton);
 }
 
-function saveCover () {
-savedCovers.push(currentCover);
-//   savedCovers.push(book1);
-//   // covers.push(userCover.value);
-//   // descriptors.push(userDescriptor1.value);
-//   // descriptors.push(userDescriptor2.value);
+function saveCover() {
+  var currentCover = new Cover(
+    //currentTitle.innerText
+    // userCover.value,
+    //userTitle.value,
+    currentImage.src,
+    currentTitle.innerText,
+    currentTagLine1.innerText,
+    currentTagLine2.innerText,
+    // userDescriptor1.value,
+    // userDescriptor2.value,
+  );
+  // console.log(currentCover);
+  savedCovers.push(currentCover);
+  // currentCover(currentTitle.innerText)
+  console.log(savedCovers);
+  //   savedCovers.push(book1);
+  //   // covers.push(userCover.value);
+  //   // descriptors.push(userDescriptor1.value);
+  //   // descriptors.push(userDescriptor2.value);
+  //return Cover
 }
 
 function viewSavedCovers() {
@@ -101,9 +125,9 @@ function viewSavedCovers() {
   hideElement(showFormView);
   showElement(savedView);
   hideElement(randomCoverButton);
-  hideElement(saveCoverButton)
+  hideElement(saveCoverButton);
   showElement(homeButton);
-  return savedCovers
+  return savedCovers;
 }
 
 function viewHome() {
@@ -116,10 +140,10 @@ function viewHome() {
 }
 
 function randomCover() {
-currentTitle.innerText = getRandomBookInfo(titles);
-currentImage.src = getRandomBookInfo(covers);
-currentTagLine1.innerText = getRandomBookInfo(descriptors);
-currentTagLine2.innerText = getRandomBookInfo(descriptors);
+  currentTitle.innerText = getRandomBookInfo(titles);
+  currentImage.src = getRandomBookInfo(covers);
+  currentTagLine1.innerText = getRandomBookInfo(descriptors);
+  currentTagLine2.innerText = getRandomBookInfo(descriptors);
 }
 
 function getRandomBookInfo(bookInfo) {
